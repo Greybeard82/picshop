ee48789aa1781f76410fb0d1444db9f3fa244ef7
Greybeard82 <davidvicentemartins@gmail.com>
2020-02-09 18:32:09 +0100

    Second commit. Created and added validations to models. Created controllers, views

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,11 @@
 class Photo < ApplicationRecord
   belongs_to :post
+  validates :name, presence: true,
+                    length: { minimum: 5 , maximum: 30 }
+            :url, presence: true,
+            :price, presence: true
 end
+
+
+# validates :title, presence: true,
+#                     length: { minimum: 5 }
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
-  has_many :comments
+  has_many :photos
+  validates :title, presence: true,
+                    length: { maximum: 140 }
+            :text, length: { minimum: 10, maximum: 500 }
 end
